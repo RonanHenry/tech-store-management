0bc7e0a7d76ab1329da57e1696e8fb50b59117ba
Ronan Henry <henry.ronan94@gmail.com>
2016-08-11 03:54:46 +0200

    Added Utility class

---
+++ TechStoreWpf/Helpers/Utility.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using TechStoreWpf.Views;
+
+namespace TechStoreWpf.Helpers
+{
+    /// <summary>
+    /// Defines utility methods.
+    /// </summary>
+    public static class Utility
+    {
+        /// <summary>
+        /// Finds the parent object with a specific type and an optional name of a child element.
+        /// </summary>
+        /// <typeparam name="T">Type of the parent.</typeparam>
+        /// <param name="child">Child element from which to retrieve the parent.</param>
+        /// <param name="parentName">Name of the parent element.</param>
+        /// <returns></returns>
+        public static T FindParent<T>(DependencyObject child, string parentName = null) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            // End of tree reached
+            if (parentObject == null)
+                return null;
+
+            T parent = parentObject as T;
+
+            if (!string.IsNullOrEmpty(parentName)) // Search by type and name
+            {
+                if (parent != null)
+                {
+                    var frameworkElement = parent as FrameworkElement;
+
+                    if (frameworkElement != null && frameworkElement.Name == parentName)
+                    {
+                        return parent;
+                    }
+                    else
+                    {
+                        return FindParent<T>(parentObject, parentName);
+                    }
+                }
+                else
+                {
+                    return FindParent<T>(parentObject, parentName);
+                }
+            }
+            else // Search by type only
+            {
+                if (parent != null)
+                {
+                    return parent;
+                }
+                else
+                {
+                    return FindParent<T>(parentObject);
+                }
+            }
+        }
+    }
+}
