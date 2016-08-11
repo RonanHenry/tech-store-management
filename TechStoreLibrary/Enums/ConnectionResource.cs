6b44f2494a23b064f6407d64748d55685d8e59e7
Ronan Henry <henry.ronan94@gmail.com>
2016-08-11 03:44:10 +0200

    Added API MySQL database resource

--- TechStoreLibrary/Enums/ConnectionResource.cs
+++ TechStoreLibrary/Enums/ConnectionResource.cs
@@ -11,7 +11,9 @@ namespace TechStoreLibrary.Enums
     {
         [StringValue("http://127.0.0.1:5000/api/")]
         LOCALAPI = 1,
-        [StringValue("Server=localhost;Port=3306;Database=local_tech_store;Uid=root;Pwd=''")]
-        LOCALMYSQL = 2
+        [StringValue("Server=localhost;Port=3306;Database=rh_api_tech_store;Uid=root;Pwd=''")]
+        APIMYSQL = 2,
+        [StringValue("Server=localhost;Port=3306;Database=rh_local_tech_store;Uid=root;Pwd=''")]
+        LOCALMYSQL = 3
     }
 }
