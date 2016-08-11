1cbfb8a643ba0a740d50bce0b9821c850fbbc039
Ronan Henry <henry.ronan94@gmail.com>
2016-08-11 04:00:41 +0200

    Added Layout view model and modified menu bar

---
+++ TechStoreWpf/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TechStoreWpf.Helpers;
+using TechStoreWpf.ViewModels.Base;
+using TechStoreWpf.Views;
+
+namespace TechStoreWpf.ViewModels
+{
+    public class LayoutViewModel : BaseViewModel
+    {
+        #region Attributes
+        private LayoutView layoutView;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Layout view.
+        /// </summary>
+        public LayoutView LayoutView
+        {
+            get
+            {
+                return layoutView;
+            }
+            set
+            {
+                layoutView = value;
+            }
+        }
+
+        public ICommand AddWorkerCommand { get; private set; }
+        public ICommand AddCustomerCommand { get; private set; }
+        #endregion
+
+        #region Constructors
+        public LayoutViewModel(LayoutView layoutView)
+        {
+            LayoutView = layoutView;
+
+            AddWorkerCommand = new RelayCommand(ExecAddWorker, CanAdd);
+            AddCustomerCommand = new RelayCommand(ExecAddCustomer, CanAdd);
+        }
+        #endregion
+
+        #region Methods
+        private bool CanAdd(object obj)
+        {
+            return true;
+        }
+
+        private void ExecAddWorker(object obj)
+        {
+            LayoutView.ContentFrame.NavigationService.Navigate(new WorkerView());
+        }
+
+        private void ExecAddCustomer(object obj)
+        {
+            LayoutView.ContentFrame.NavigationService.Navigate(new CustomerView());
+        }
+        #endregion
+    }
+}
--- TechStoreWpf/Views/LayoutView.xaml.cs
+++ TechStoreWpf/Views/LayoutView.xaml.cs
@@ -12,26 +12,46 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TechStoreWpf.ViewModels;
 
 namespace TechStoreWpf.Views
 {
     /// <summary>
     /// Interaction logic for Layout.xaml
     /// </summary>
-    public partial class Layout : Page
+    public partial class LayoutView : Page
     {
         #region Attributes
-
+        private LayoutViewModel layoutViewModel;
         #endregion
 
         #region Properties
+        /// <summary>
+        /// ViewModel of the view.
+        /// </summary>
+        public LayoutViewModel LayoutViewModel
+        {
+            get
+            {
+                return layoutViewModel;
+            }
+            set
+            {
+                layoutViewModel = value;
+            }
+        }
+
         public BrushConverter BrushConverter { get; set; }
         #endregion
 
         #region Constructors
-        public Layout()
+        public LayoutView()
         {
             InitializeComponent();
+
+            LayoutViewModel = new LayoutViewModel(this);
+            DataContext = LayoutViewModel;
+
             BrushConverter = new BrushConverter();
         }
         #endregion
@@ -48,7 +68,6 @@ namespace TechStoreWpf.Views
             StaffMenu.Background = (Brush)BrushConverter.ConvertFrom("#FF565656");
             ContentFrame.NavigationService.Navigate(new WorkerListView());
         }
-        #endregion
 
         /// <summary>
         /// Loads the customer list view in the frame.
@@ -61,5 +80,41 @@ namespace TechStoreWpf.Views
             CustomerMenu.Background = (Brush)BrushConverter.ConvertFrom("#FF565656");
             ContentFrame.NavigationService.Navigate(new CustomerListView());
         }
+
+        private void Control_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StatusBarTxt.Text = "Ready";
+        }
+
+        private void AddWorker_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StatusBarTxt.Text = "Add an employee";
+        }
+
+        private void AddCustomer_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StatusBarTxt.Text = "Add a customer";
+        }
+
+        private void AppAbout_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StatusBarTxt.Text = "See credits";
+        }
+
+        private void AppExit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StatusBarTxt.Text = "Exit the application";
+        }
+
+        private void AppExit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationWindow.GetWindow(this).Close();
+        }
+
+        private void AppAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Tech Store Management\n\nMade with love by Ronan Henry", "About");
+        }
+        #endregion
     }
 }
