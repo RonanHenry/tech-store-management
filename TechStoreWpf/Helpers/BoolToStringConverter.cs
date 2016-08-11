bda9417a83764a4c45c0f64cfe392055e2a7ac26
Ronan Henry <henry.ronan94@gmail.com>
2016-08-11 15:46:00 +0200

    Added Bool to String converter

---
+++ TechStoreWpf/Helpers/BoolToStringConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TechStoreWpf.Helpers
+{
+    [ValueConversion(typeof(bool), typeof(string))]
+    public class BoolToStringConverter : IValueConverter
+    {
+        #region Attributes
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return "Yes";
+            else
+                return "No";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return true;
+        }
+        #endregion
+    }
+}
